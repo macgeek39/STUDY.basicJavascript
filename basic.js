138f2bf0bc6b9bd32eb473ddc889bef7bbea7dd5
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-22 07:18:19 -0600

    Added user accept input

--- basic.js
+++ basic.js
@@ -3,5 +3,11 @@
 
 //window.alert(`This is an alert`);
 
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I love pizza`;
+//document.getElementById("myH1").textContent = `Hello`;
+//document.getElementById("myP").textContent = `I love pizza`;
+
+document.getElementById("mySubmit").onclick = function()
+{
+    username = document.getElementById("myText").value;
+    document.getElementById("update").textContent = `Hello ${username}`;
+}
