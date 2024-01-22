22ea315b0006609ee1c4e218d98ac737da74556f
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-22 07:02:22 -0600

    Added document get element by id

--- basic.js
+++ basic.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+//console.log("Hello World");
+//console.log(`I like Pizza`);
+
+//window.alert(`This is an alert`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I love pizza`;
